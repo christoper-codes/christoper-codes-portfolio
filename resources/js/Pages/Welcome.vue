ccc31acc3cc649755634ede7c48052e21e7be147
Christoper Patiño <118621756+christoper-codes@users.noreply.github.com>
2026-01-01 21:47:10 -0600

    Simplify welcome

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
-import TypeIt from "typeit";
 import { nextTick, onMounted, ref } from 'vue';
-import LightLayout from '@/Layouts/LightLayout.vue';
 import NavLink from '@/Components/NavLink.vue';
 
 const TransitionState = ref(false);
@@ -10,38 +8,16 @@ const TransitionState = ref(false);
 onMounted(async () => {
   TransitionState.value = true;
   await nextTick();
-
-  new TypeIt("#animatedText", {
-    speed: 75,
-    loop: true,
-    breakLines: false,
-    afterComplete: (instance) => {
-      instance
-        .pause(1000)
-        .type("Christoper")
-        .pause(2000)
-        .delete()
-        .pause(1000)
-        .type("an engineer")
-        .pause(2000)
-        .delete()
-        .pause(1000)
-        .type("a full-stack")
-        .pause(2000)
-        .delete()
-        .go();
-    }
-  }).go();
 });
 </script>
 
 <template>
     <Head title="Welcome" />
-    <LightLayout />
+
     <!-- masthead -->
      <transition name="fade" mode="out-in">
         <section v-if="TransitionState" id="masthead" class="h-screen w-full relative overflow-hidden">
-            <div class="min-h-full w-full lg:max-w-[80%] flex flex-col items-center justify-center text-center mx-auto gap-5 text-xl">
+            <div class="min-h-full w-full lg:max-w-[80%] flex flex-col items-start mx-auto gap-10">
                 <div class="z-10 relative font-bold title-welcome text-2xl">
                     <span>Hello It's </span>
                     <span id="animatedText" class="text-red-600"></span>
