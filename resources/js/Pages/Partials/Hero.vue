829363f33f2c18a23cce1b1514dd3b70a256b9b0
Christoper Patiño <118621756+christoper-codes@users.noreply.github.com>
2026-01-01 22:09:25 -0600

    Add hero partial

---
+++ resources/js/Pages/Partials/Hero.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+<template>
+    <div class="min-h-full w-full lg:max-w-3xl flex flex-col items-start mx-auto">
+        <div class="flex flex-col gap-3">
+            <h1 class="text-2xl uppercase">Christoper Patiño Santos</h1>
+            <h2>Software engineer</h2>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,26 +2,26 @@
 import { Head } from '@inertiajs/vue3';
 import { nextTick, onMounted, ref } from 'vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
+import Hero from './Partials/Hero.vue';
 
 const TransitionState = ref(false);
 
-onMounted(async () => {
+onMounted(() => {
   TransitionState.value = true;
-  await nextTick();
 });
 </script>
 
 <template>
     <Head title="Welcome" />
-    <GuestLayout />
-    <!-- masthead -->
-     <transition name="fade" mode="out-in">
-        <section v-if="TransitionState" id="masthead" class="h-screen w-full relative overflow-hidden">
-            <div class="min-h-full w-full lg:max-w-[80%] flex flex-col items-start mx-auto gap-10">
-                <div class="z-10 relative font-bold title-welcome text-2xl">
-                    <span>Hello It's </span>
-                </div>
-            </div>
+    <transition name="fade" mode="out-in">
+        <section v-if="TransitionState">
+            <GuestLayout />
+        </section>
+    </transition>
+
+    <transition name="fade" mode="out-in">
+        <section v-if="TransitionState">
+            <Hero />
         </section>
     </transition>
 </template>
