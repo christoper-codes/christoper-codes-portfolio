bd5ae5b0d7bbf2622739b1e43a39b84fd079da99
Christoper Patiño <118621756+christoper-codes@users.noreply.github.com>
2026-01-01 21:58:18 -0600

    Refactor routes

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
 import { nextTick, onMounted, ref } from 'vue';
-import NavLink from '@/Components/NavLink.vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 
 const TransitionState = ref(false);
@@ -21,14 +20,6 @@ onMounted(async () => {
             <div class="min-h-full w-full lg:max-w-[80%] flex flex-col items-start mx-auto gap-10">
                 <div class="z-10 relative font-bold title-welcome text-2xl">
                     <span>Hello It's </span>
-                    <span id="animatedText" class="text-red-600"></span>
-                </div>
-
-                <div class="text-center flex gap-5">
-                    <NavLink :href="route('welcome')" :active="route().current('welcome')"><span>Welcome</span></NavLink>
-                    <NavLink :href="route('about')" :active="route().current('about')"><span>About</span></NavLink>
-                    <NavLink :href="route('uses')" :active="route().current('uses')"><span>Uses</span></NavLink>
-                    <NavLink :href="route('contact')" :active="route().current('contact')"><span>Let's talk</span></NavLink>
                 </div>
             </div>
         </section>
--- routes/web.php
+++ routes/web.php
@@ -5,31 +5,15 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', fn() => Inertia::render('Welcome'))->name('welcome');
-Route::get('/about', fn() => Inertia::render('About'))->name('about');
-Route::get('/uses', fn() => Inertia::render('Uses'))->name('uses');
-Route::get('/contact', fn() => Inertia::render('Contact'))->name('contact');
-
-Route::get('/migrate-fresh', function () {
-
-    Artisan::call('migrate:fresh');
-
-    return "migrate-fresh";
-
-});
-
 
 Route::get('/storage-link', function () {
-
     Artisan::call('storage:link');
 
     return "storage-link";
-
 });
 
 Route::get('/optimize-clear', function () {
-
     Artisan::call('optimize:clear');
 
     return "optimize-clear";
-
 });
