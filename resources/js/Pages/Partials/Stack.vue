4d30ade2953567144882634e69e584db5ba54960
Christoper Patiño <118621756+christoper-codes@users.noreply.github.com>
2026-01-03 17:57:27 -0600

    Refactor stack partial hover

--- resources/js/Pages/Partials/Stack.vue
+++ resources/js/Pages/Partials/Stack.vue
@@ -1,13 +1,13 @@
 <template>
     <section id="stack">
-        <h2 class="text-2xl uppercase mb-5">Stacks</h2>
+        <h2 class="text-2xl uppercase mb-5">Stack</h2>
         <main class="flex flex-col md:flex-row md:items-center gap-7">
             <div class="flex flex-col gap-2">
                 <div class="flex items-center gap-2">
-                    <div class="inline bg-neutral-200 dark:bg-neutral-900 py-2 px-4 rounded-md text-center hover:scale-105 transition-transform duration-500">
+                    <div class="inline bg-neutral-200 dark:bg-neutral-900 py-2 px-4 rounded-md text-center">
                         TALL
                     </div>
-                    <div class="inline bg-neutral-200 dark:bg-neutral-900 py-2 px-4 rounded-md text-center hover:scale-105 transition-transform duration-500">
+                    <div class="inline bg-neutral-200 dark:bg-neutral-900 py-2 px-4 rounded-md text-center">
                         VILT
                     </div>
                 </div>
@@ -15,7 +15,7 @@
             </div>
             <div class="flex flex-col gap-2">
                 <div class="flex items-center gap-2">
-                    <div class="inline bg-neutral-200 dark:bg-neutral-900 py-2 px-4 rounded-md text-center hover:scale-105 transition-transform duration-500">
+                    <div class="inline bg-neutral-200 dark:bg-neutral-900 py-2 px-4 rounded-md text-center">
                         PyData
                     </div>
                 </div>
