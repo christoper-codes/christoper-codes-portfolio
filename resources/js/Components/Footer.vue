8ddfd6542e9df3c7754d75dafbc6e5ec8c8a816b
Christoper Patiño <christopher.code.git@gmail.com>
2026-02-08 19:41:13 -0600

    Refactor link footer

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -4,6 +4,6 @@
 
 <template>
     <footer class="text-center w-full text-sm">
-        © {{ year }} Christoper Patiño. This website is open source.
+        © {{ year }} Christoper Patiño. This website is <a href="https://github.com/christoper-codes/christoper-codes-portfolio" target="_blank" class="underline font-bold">open source</a>
     </footer>
 </template>
