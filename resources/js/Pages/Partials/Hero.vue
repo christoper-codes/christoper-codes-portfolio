6e3f03ce8dadb00b4b9ed94653b73f1e9660f1e0
Christoper Patiño <christopher.code.git@gmail.com>
2026-02-08 20:02:05 -0600

    Add instagram link

--- resources/js/Pages/Partials/Hero.vue
+++ resources/js/Pages/Partials/Hero.vue
@@ -18,6 +18,9 @@
                 <a href="https://www.linkedin.com/in/christoper-pati%C3%B1o/" target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" class="animate-fade animate-delay-[1500ms] animate-once w-7 fill-white! text-white! h-auto" aria-label="LinkedIn" role="img" viewBox="0 0 512 512" fill="#ffffff"><rect width="512" height="512" rx="15%" fill="#0077b5"/><circle cx="142" cy="138" r="37"/><path stroke="#ffffff" stroke-width="66" d="M244 194v198M142 194v198"/><path d="M276 282c0-20 13-40 36-40 24 0 33 18 33 45v105h66V279c0-61-32-89-76-89-34 0-51 19-59 32"/></svg>
                 </a>
+                <a href="https://www.instagram.com/_christosantos/" target="_blank">
+                    <img class="animate-fade animate-delay-[1700ms] animate-once w-[30px] h-auto" src="/assets/img/instagram.png" alt="Profile picture" />
+                </a>
             </div>
         </div>
         <div class="animate-fade-up animate-delay-[900ms] animate-once w-full md:w-[70%] rounded-[30px] h-72 overflow-hidden relative">
